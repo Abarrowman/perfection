51330cc3d8d3f854f3d1cde06d7e8a21e1dcc5bb
Adam Barrowman <adambarrowman@yahoo.ca>
2019-08-17 22:58:01 -0700

    Made code more generic.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <typeinfo>
 #include <string_view>
 #include <cstdint>
-
 #include <vector>
 #include <string>
 #include <limits>
-
 #include <algorithm>
 #include <array>
+#include <cassert>
+#include <optional>
 
 template<typename L>
 constexpr bool is_different_from_others() {
@@ -99,7 +99,7 @@ void inner_add_to_table(std::array<Enum, Count>& table) {
 
   auto hash = hash_string_constant<First, Count, Param>();
   constexpr uint64_t hash_value = decltype(hash)::value();
-  std::cout << First::value() << " -> " << hash_value << "\n";
+  //std::cout << First::value() << " -> " << hash_value << "\n";
 
   table[hash_value] = First::enum_value();
 
@@ -110,9 +110,6 @@ template<typename Enum, uint64_t Count, uint64_t Param, typename First, typename
 void add_to_table(std::array<Enum, Count>& table, varriadic_holder<First, T...>, type_value<uint64_t, Param>) {
   inner_add_to_table<Enum, Count, Param, First, T...>(table);
 }
-
-
-
 
 template<typename First, typename... T>
 constexpr auto merge(First first, varriadic_holder<T...> others) {
@@ -162,6 +159,23 @@ static constexpr uint64_t find_perfect_param(varriadic_holder<L...> vh, type_val
   return inner_find_perfect_param<Start, decltype(vh)::size(), L...>();
 }
 
+template<size_t Count>
+void add_strings(std::array<std::string_view, Count>&, varriadic_holder<>, size_t) {}
+
+template<size_t Count, typename F, typename... T>
+void add_strings(std::array<std::string_view, Count>& strings, varriadic_holder<F, T...>, size_t index) {
+  strings[index] = F::value();
+  add_strings(strings, varriadic_holder<T...>{}, index + 1);
+}
+
+template<size_t Count, typename... T>
+void add_strings(std::array<std::string_view, Count>& strings,  varriadic_holder<T...> vh) {
+  add_strings(strings, vh, 0);
+}
+
+constexpr uint64_t hash_string(std::string_view str, uint64_t space, uint64_t param) {
+  return ((hash_string_view(str) * param) % 15485863) % space;
+}
 
 struct phonetics {
   enum class PHONETICS { ALFA, BRAVO, CHARLIE, DELTA, ECHO, FOXTROT, GOLF, HOTEL };
@@ -175,7 +189,7 @@ struct phonetics {
   DECLARE_ENUM_HOLDER(PHONETICS, HOTEL, "hotel")
   using TYPES = varriadic_holder<ALFA, BRAVO, CHARLIE, DELTA, ECHO, FOXTROT, GOLF, HOTEL>;
   using HASH_TABLE_T = std::array<PHONETICS, TYPES::size()>;
-
+  using STRINGS_T = std::array<std::string_view, TYPES::size()>;
 
   static constexpr size_t count() {
     return TYPES::size();
@@ -185,7 +199,7 @@ struct phonetics {
     return find_perfect_param(TYPES{}, type_value<uint64_t, 0>{});
   }
 
-  static HASH_TABLE_T const& get_hash_table() {
+  static HASH_TABLE_T const& hash_table() {
     static HASH_TABLE_T table;
     static bool populated = false;
     if (!populated) {
@@ -194,16 +208,28 @@ struct phonetics {
     return table;
   }
 
-  /*std::ostream& operator<<(std::ostream& os, PHONETICS const& p) {
-    uint64_t idx = static_cast<uint64_t>(p);
-    std::string_view name;
-    return os;
-  }*/
-};
+  static STRINGS_T const& strings() {
+    static STRINGS_T strings;
+    static bool populated = false;
+    if (!populated) {
+      add_strings(strings, TYPES{});
+    }
+    return strings;
+  }
 
-uint64_t hash_string(std::string_view str, uint64_t space, uint64_t param) {
-  return ((hash_string_view(str) * param) % 15485863) % space;
-}
+  static constexpr uint64_t perfect_hash(std::string_view sv) {
+    return hash_string(sv, count(), hash_param());
+  }
+
+  static constexpr std::optional<PHONETICS> find(std::string_view sv) {
+    auto value = hash_table()[static_cast<size_t>(perfect_hash(sv))];
+    if (strings()[static_cast<size_t>(value)] == sv) {
+      return value;
+    } else {
+      return std::nullopt;
+    }
+  }
+};
 
 bool are_all_different(std::vector<uint64_t> const& hashes) {
   for (std::vector<uint64_t>::const_iterator it = hashes.begin(); it != hashes.end(); ++it) {
@@ -214,12 +240,13 @@ bool are_all_different(std::vector<uint64_t> const& hashes) {
   return true;
 }
 
-uint64_t find_perfect_hash(std::vector<std::string_view> const& strings, std::vector<uint64_t>& hashes) {
+template<size_t Count>
+uint64_t find_perfect_hash(std::array<std::string_view, Count> const& strings, std::vector<uint64_t>& hashes) {
   uint64_t max = std::numeric_limits<uint64_t>::max();
   for (uint64_t param = 0; param < max; ++param) {
-    if (param % 1000 == 0) {
+    /*if (param % 1000 == 0) {
       std::cout << "Trying param=" << param << "\n";
-    }
+    }*/
     for (size_t j = 0; j < strings.size(); ++j) {
       hashes[j] = hash_string(strings[j], strings.size(), param);
     }
@@ -230,37 +257,30 @@ uint64_t find_perfect_hash(std::vector<std::string_view> const& strings, std::ve
   return max;
 }
 
-void add_strings(std::vector<std::string_view>&, varriadic_holder<>) {}
-
-template<typename F, typename... T>
-void add_strings(std::vector<std::string_view>& vec, varriadic_holder<F, T...>) {
-  vec.emplace_back(F::value());
-  add_strings(vec, varriadic_holder<T...>{});
-}
-
-template<typename... T>
-std::vector<std::string_view> strings_from_list(varriadic_holder<T...> vh) {
-  std::vector<std::string_view> vec;
-  add_strings(vec, vh);
-  return vec;
+std::ostream& operator<<(std::ostream& os, phonetics::PHONETICS const& p) {
+  size_t value = static_cast<size_t>(p);
+  std::string_view str = phonetics::strings()[value];
+  os << "PHONETICS(str=\"" << str << "\" value=" << value << ")";
+  return os;
 }
 
 int main() {
-  phonetics::HASH_TABLE_T const& table = phonetics::get_hash_table();
-
-
-  std::vector<std::string_view> strings = strings_from_list(phonetics::TYPES{});
+  phonetics::HASH_TABLE_T const& table = phonetics::hash_table();
+  phonetics::STRINGS_T const& strings = phonetics::strings();
 
   std::vector<uint64_t> hashes;
   hashes.resize(strings.size());
 
   uint64_t param = find_perfect_hash(strings, hashes);
-
+  assert(param == phonetics::hash_param());
   std::cout << "Found the perfect hash function param=" << param << "\n";
   for (size_t j = 0; j < strings.size(); ++j) {
-    std::cout << "strings[j] = " << strings[j] << "  hashes[j] = " << hashes[j] << "\n";
-//    std::cout << "strings[j] = " << strings[j] << "  hashes[j] = " << hashes[j] << "  table[hashes[j]] = " << table[hashes[j]] << "\n";
+    auto phon = phonetics::find(strings[j]);
+    assert(phon.has_value());
+    std::cout << "strings[j] = " << strings[j] << "  hashes[j] = " << hashes[j] << "  phonetics.find(strings[j]) = " << phon.value() << "\n";
   }
+
+  assert(phonetics::find("cow") == std::nullopt);
 
   return 0;
 }
